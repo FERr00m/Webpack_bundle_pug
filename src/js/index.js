0d070424c6a4a07fd2e11bf32ecb1aaa345be42d
Алексей Н. Мельников <u183@stekspb.ru>
2022-07-26 17:49:44 +0300

    add GSAP

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,14 @@ function requireAll(r) {
 import '@/styles/style';
 
 // JS Modules
+import vueCustom from '@/js/modules/vue-custom';
 import other from '@/js/modules/other';
 import jq from '@/js/modules/jq';
-import vueCustom from '@/js/modules/vue-custom';
-
-//import ad from '@/svg/address-blue.svg';
+import hoverEffectBtns from '@/js/modules/hover-effect-btns';
 
 requireAll(require.context('@/svg/', true, /\.svg$/));
 
+vueCustom();
 other();
 jq();
-vueCustom();
+hoverEffectBtns();
--- src/js/modules/other.js
+++ src/js/modules/other.js
@@ -1,5 +1,7 @@
 const other = () => {
   console.log('I am Other module');
+  gsap.to(".title", {duration: 3, rotation: 360});
+  gsap.to("button", {duration: 4.3, rotation: 360});
 };
 
 export default other;
