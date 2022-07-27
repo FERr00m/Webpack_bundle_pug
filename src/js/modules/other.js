f0ab0c3afc966901862351a7eb50e018dd6613b1
Алексей Н. Мельников <u183@stekspb.ru>
2022-07-27 17:34:01 +0300

    add features...

--- src/js/modules/other.js
+++ src/js/modules/other.js
@@ -1,7 +1,5 @@
 const other = () => {
   console.log('I am Other module');
-  gsap.to(".title", {duration: 3, rotation: 360});
-  gsap.to("button", {duration: 4.3, rotation: 360});
 };
 
 export default other;
--- src/js/modules/vue-custom.js
+++ src/js/modules/vue-custom.js
@@ -4,7 +4,7 @@ const vueCustom = () => {
   createApp({
     data() {
       return {
-        message: 'With Vue!',
+        message: 'With Vue! & GSAP',
         counter: 0,
         visible: true,
       };
@@ -12,17 +12,32 @@ const vueCustom = () => {
     methods: {
       vueClick() {
         this.counter++;
+        if (this.counter == 3) {
+          let t1 = gsap.timeline();
+          t1.to('.title', {
+            duration: 3,
+            rotation: 360,
+            x: 300,
+            ease: 'expo',
+            transform: 'scale(.5)',
+          });
+          t1.to('.title', { duration: 1, x: 0, color: 'green' });
+          t1.to('.title', {
+            duration: 1,
+            rotation: 360,
+            transform: 'scale(1.5)',
+          });
+          gsap.to('body', { duration: 1, backgroundColor: 'black', delay: 3 });
+        }
       },
       mainFontSizeMin() {
         var style = getComputedStyle(document.querySelector('html')).fontSize;
         style = parseFloat(style) - 1;
-        console.log(parseFloat(style));
         document.querySelector('html').style.fontSize = style + 'px';
       },
       mainFontSizeMax() {
         var style = getComputedStyle(document.querySelector('html')).fontSize;
         style = parseFloat(style) + 1;
-        console.log(parseFloat(style));
         document.querySelector('html').style.fontSize = style + 'px';
       },
     },
