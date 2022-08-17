8b313ac3da65fe00023c65b66d348f56cd6b4fd5
Алексей Н. Мельников <u183@stekspb.ru>
2022-08-17 17:32:26 +0300

    add lazyload with skeleton image

--- src/js/modules/jq.js
+++ src/js/modules/jq.js
@@ -4,6 +4,13 @@ const jq = () => {
     'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js'
   );
   console.log(myFunctions.classof({}));
+
+  const lazyLoadInstance = new LazyLoad();
+
+  const scene = document.getElementById('scene');
+  if (scene) {
+    const parallaxInstance = new Parallax(scene);
+  }
 };
 
 export default jq;
