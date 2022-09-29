e43930aceae557735e161cd217f0a72a73ef4779
Алексей Н. Мельников <u183@stekspb.ru>
2022-09-29 16:50:30 +0300

    materialize init

--- src/js/index.js
+++ src/js/index.js
@@ -6,21 +6,16 @@ function requireAll(r) {
 import '@/libs/css/normalize';
 import '@/libs/css/fancybox';
 import '@/libs/css/splide.min';
+import '@/libs/css/materialize.min';
 // Main css
 import '@/styles/style';
 
 // JS Modules
-import vueCustom from '@/js/modules/vue-custom';
 import MyFunctions from '@/js/modules/MyFunctions';
-import other from '@/js/modules/other';
 import jq from '@/js/modules/jq';
-import hoverEffectBtns from '@/js/modules/hover-effect-btns';
 
 requireAll(require.context('@/svg/', true, /\.svg$/));
 
 window.myFunctions = new MyFunctions();
 
-vueCustom();
-other();
 jq();
-hoverEffectBtns();
--- src/js/modules/jq.js
+++ src/js/modules/jq.js
@@ -1,16 +1,11 @@
 const jq = () => {
-  console.log('JQuery is working! ', $('nav'));
-  myFunctions.importScript(
-    'https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js'
-  );
+  $(function () {
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+  });
   console.log(myFunctions.classof({}));
 
   const lazyLoadInstance = new LazyLoad();
-
-  const scene = document.getElementById('scene');
-  if (scene) {
-    const parallaxInstance = new Parallax(scene);
-  }
 };
 
 export default jq;
