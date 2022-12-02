1e88ff86dc59d0ee4a9b1a8b6800c1023ae336c5
FERr00m <59802905+FERr00m@users.noreply.github.com>
2022-12-02 10:23:49 +0300

    Update MyFunctions.js

--- src/js/modules/MyFunctions.js
+++ src/js/modules/MyFunctions.js
@@ -292,6 +292,31 @@ class MyFunctions {
       console.log(`Строка '${string}' идентична строке '${string2}'.`);
     }
   }
+  //=======================================
+  //
+  //
+  //=======================================
+  // Массив объектов сортирует 
+  // по произвольному свойству объекта
+  // let data = [
+  //   {name: "Zachary", age: 28},
+  //   {name: "Nicholas", age: 29}
+  // ];
+  // data.sort(createComparisonFunction("name"));
+  // console.log(data[0].name); // Nicholas
+  createComparisonFunction(propertyName) {
+    return function(object1, object2) {
+      let value1 = object1[propertyName];
+      let value2 = object2[propertyName];
+      if (value1 < value2) {
+        return -1;
+      } else if (value1 > value2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
 }
 
 export default MyFunctions;
